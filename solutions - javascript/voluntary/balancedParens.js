46ff1312eb5deadcc7523366d475e9bf3c027812
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-08-14 20:33:15 +0700

    add balanced parentheses function

---
+++ solutions - javascript/voluntary/balancedParens.js
@@ -0,0 +1,15 @@
+// create a function that check whether a string containing set of parentheses is balance
+// ex: ()()() -> true, )()()( -> false, ()()()( -> false
+
+function balancedParens(string){
+  return !string.split("").reduce(function(previous, char){
+    if(previous < 0) {return previous; }
+    if(char === "(") {return ++previous; }
+    if(char === ")") {return --previous; }
+    return previous;
+  }, 0);
+}
+
+console.log(balancedParens("()()()")); // true
+console.log(balancedParens(")()()(")); // false
+console.log(balancedParens("()()(")); // false
