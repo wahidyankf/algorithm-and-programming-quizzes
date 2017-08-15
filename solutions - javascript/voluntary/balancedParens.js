3440468ad0d68941ddab2265342b1db34de19260
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-08-15 08:41:34 +0700

    edit balanceparens format

--- solutions - javascript/voluntary/balancedParens.js
+++ solutions - javascript/voluntary/balancedParens.js
@@ -6,12 +6,15 @@ function balancedParens(string) {
 		if (previous < 0) {
 			return previous;
 		}
+
 		if (char === '(') {
 			return ++previous;
 		}
+
 		if (char === ')') {
 			return --previous;
 		}
+
 		return previous;
 	}, 0);
 }
